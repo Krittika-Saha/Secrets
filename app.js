288e0936fc6aaf825c08cea40552c28d8a8bcc07
Krittika-Saha <krittika.saha.dev@gmail.com>
2020-10-27 22:45:50 +0530

    Using env variables

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -6,6 +7,8 @@ const mongoose = require("mongoose");
 const encrypt = require('mongoose-encryption');
 
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -20,8 +23,8 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-const secret = 'Dontchadarehackme';
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
 
 
 const User = new mongoose.model("User", userSchema);
@@ -30,15 +33,12 @@ const User = new mongoose.model("User", userSchema);
 app.get('/', function(req, res){
   res.render('home');
 });
-
 app.get('/login', function(req, res){
   res.render('login');
 });
-
 app.get('/register', function(req, res){
   res.render('register');
 });
-
 app.post('/register', function(req, res){
   const newUser = new User({
     email: req.body.username,
@@ -85,4 +85,4 @@ app.post('/login', function(req, res) {
 
 app.listen(1000, function() {
   console.log("Server started on port 1000.")
-})
+});
